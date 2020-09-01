2ff8b4df93047ba7e6e6836be68ccca5bfc85e43
Otakoe <vitalylarc@gmail.com>
2020-09-01 23:29:17 +0300

    Start

---
+++ src/main/java/com/domain/enums/UserRole.java
@@ -0,0 +1,5 @@
+package com.domain.enums;
+
+public enum UserRole {
+    ADMIN,USER
+}
---
+++ src/main/java/com/service/UserService.java
@@ -0,0 +1,15 @@
+package com.service;
+
+import com.domain.User;
+import com.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+
+}
